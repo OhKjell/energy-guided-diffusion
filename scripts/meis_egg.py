7aab6200135f25bd53726ac17036d55ea3ae82bb
davidhiroki.ortelt <davidhiroki.ortelt@stud.uni-goettingen.de>
2023-06-24 20:51:50 +0200

    egg

--- scripts/meis_egg.py
+++ scripts/meis_egg.py
@@ -25,7 +25,7 @@ num_timesteps = 100
 energy_scale = 5  # 20
 seeds = [0, 1, 2]
 norm_constraint = 25  # 25
-model_type = "color"  #'task_driven' #or 'v4_multihead_attention'
+model_type = "task_driven"  #'task_driven' #or 'v4_multihead_attention'
 
 
 def do_run(model, energy_fn, desc="progress", grayscale=False, seed=None):
