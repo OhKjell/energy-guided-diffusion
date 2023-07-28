92729b602c957597f2c068c20e3400ffb603282f
davidhiroki.ortelt <davidhiroki.ortelt@stud.uni-goettingen.de>
2023-07-28 19:10:03 +0200

    s

--- scripts/dynamic_meis_egg.py
+++ scripts/dynamic_meis_egg.py
@@ -25,21 +25,38 @@ import shutil
 import glob
 import cv2
 
-# experiment settings
-num_timesteps = 100
-energy_scale = 5  # 20
-#energy_scale2 = 1
-seeds = [0]#np.arange(1)
-unit_seed=27#42
-norm_constraint = 25  # 25
-model_type = "task_driven"  #'task_driven' #or 'v4_multihead_attention'
-energyfunction = "MSE" #"MSE" "VGG" "None"
-number_units = 3
-number_frames = np.arange(4)
-create_vgg = True
-fps = 20
-unit_ids = None #None [id]
-#for vgg
-vgg_gray=True
-escale2 = [0, 1, 2, 3, 4, 5]
 
+from dynamic.utils.global_functions import *
+from IPython.display import Video
+from dynamic.models.helper_functions import get_model_and_dataloader, get_model_temp_reach, get_model_and_dataloader_for_nm
+from dynamic.evaluations.single_cell_performance import get_performance_for_single_cell
+from dynamic.meis.visualizer import get_model_activations
+from Python.display import Video
+from dynamic.datasets.stas import get_cell_sta, show_sta, get_sta
+import pickle
+
+
+
+
+
+directory = "models/"
+filename = "lr_0.0060_l_4_ch_[8, 16, 32, 64]_t_27_bs_16_tr_10_ik_27x(21, 21)x(21, 21)_hk_5x(5, 5)x(5, 5)_g_48.0000_gt_0.0740_l1_0.0230_l2_0.0000_sg_0.25_d_1_dt_1_hd_1-2-3_hdt_1-1-1_p_0_bn_1_s_1norm_0_fn_1_0_h_80_w_90"
+device = 'cuda'
+model_fn = 'models.FactorizedEncoder.build_trained'
+
+#build dynamic model
+
+
+dynamic_model = get_model_and_dataloader_for_nm(
+            directory,
+            filename,
+            model_fn=model_fn,
+            device=device,
+            data_dir=None, # if data_dir is None, root of the project is considered
+            test=False,
+            seed=seed,
+            data_type=data_type,
+        )
+
+
+print("yeaahh")
