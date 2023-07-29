eac05b3607b1fd1971bcb3a12c4d6d1e4e21a2c4
davidhiroki.ortelt <davidhiroki.ortelt@stud.uni-goettingen.de>
2023-07-29 14:51:55 +0200

    s

--- scripts/dynamic_meis_egg.py
+++ scripts/dynamic_meis_egg.py
@@ -27,13 +27,14 @@ import cv2
 
 
 #from dynamic.utils.global_functions import *
-from IPython.display import Video
-from dynamic.models.helper_functions import get_model_and_dataloader, get_model_temp_reach, get_model_and_dataloader_for_nm
-from dynamic.evaluations.single_cell_performance import get_performance_for_single_cell
-from dynamic.meis.visualizer import get_model_activations
-from Python.display import Video
-from dynamic.datasets.stas import get_cell_sta, show_sta, get_sta
-import pickle
+# from IPython.display import Video
+from dynamic.models.helper_functions import get_model_and_dataloader
+# get_model_temp_reach, get_model_and_dataloader_for_nm
+# from dynamic.evaluations.single_cell_performance import get_performance_for_single_cell
+# from dynamic.meis.visualizer import get_model_activations
+# from Python.display import Video
+# from dynamic.datasets.stas import get_cell_sta, show_sta, get_sta
+# import pickle
 
 
 
