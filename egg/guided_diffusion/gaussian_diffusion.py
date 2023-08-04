d6d683cb7658c7698f764f260b956e6a3e1fe58f
davidhiroki.ortelt <davidhiroki.ortelt@stud.uni-goettingen.de>
2023-08-04 22:03:01 +0200

    s

--- egg/guided_diffusion/gaussian_diffusion.py
+++ egg/guided_diffusion/gaussian_diffusion.py
@@ -628,8 +628,10 @@ class GaussianDiffusion:
                 print(f"Used GPU memory: {th.cuda.memory_allocated(device) / (1024**3):.2f} GiB")
                 out.append(output_frame)
                 print(i)
+                print(th.cuda.memory_summary(device))
                 frame.detach()
                 th.cuda.empty_cache()
+                print(th.cuda.memory_summary(device))
             
 
             energy = energy_fn(out["pred_xstart"])
