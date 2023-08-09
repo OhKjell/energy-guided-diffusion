db98388f3cc326f446c591a4b5a81c66a7cb9406
davidhiroki.ortelt <davidhiroki.ortelt@stud.uni-goettingen.de>
2023-08-09 18:42:15 +0200

    w

--- egg/guided_diffusion/gaussian_diffusion.py
+++ egg/guided_diffusion/gaussian_diffusion.py
@@ -664,6 +664,7 @@ class GaussianDiffusion:
             output = {"sample": None}
 
             x_fused = [d["sample"][0] for d in out]
+            x_fused = th.tensor(x_fused)
 #            output["sample"] = th.stack(pred_x_tensors, dim=0)
             print("#####")
             print(x_fused.shape)
