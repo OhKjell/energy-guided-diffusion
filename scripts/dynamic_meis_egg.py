3092f7536415ead5f351d0563e64d8924d64fc2a
davidhiroki.ortelt <davidhiroki.ortelt@stud.uni-goettingen.de>
2023-08-19 16:14:59 +0200

    w

--- scripts/dynamic_meis_egg.py
+++ scripts/dynamic_meis_egg.py
@@ -95,10 +95,10 @@ def dynamic_function(x):
     x = x.mean(dim=1, keepdim=True)
     print(f"SHAPE OF DYNAMIC INPUT: {x.shape}")
     print(x.dtype)
-    y = x / torch.norm(x) * norm_constraint
-    print(torch.mean(x))
-    print(torch.mean(y))
-    output = dynamic_model(y)
+    #y = x / torch.norm(x) * norm_constraint
+    #print(torch.mean(x))
+    #print(torch.mean(y))
+    output = dynamic_model(x)
     #output = x
     print(output.shape)
     #output = x
