c8e32122ce339e6d2da3bdb2cea398e3cdc70f6d
davidhiroki.ortelt <davidhiroki.ortelt@stud.uni-goettingen.de>
2023-08-20 14:15:41 +0200

    w

--- scripts/dynamic_meis_egg.py
+++ scripts/dynamic_meis_egg.py
@@ -38,7 +38,7 @@ from dynamic.meis.visualizer import get_model_activations
 # import pickle
 
 
-num_timesteps = 1100
+num_timesteps = 1000
 norm_constraint_respones = 5
 norm_constraint = 10000
 
@@ -245,7 +245,7 @@ outputs = model.sample_video(
         energy_fn=dynamic_function,
         energy_fn2=MSE_sum,
         energy_scale=3,
-        energy_scale2=5,
+        energy_scale2=10,
         num_samples=39,
         iterative = False,
         iterations=10,
