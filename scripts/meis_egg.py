663e22dcdc3d45999d611a07518b8e1c4638b440
davidhiroki.ortelt <davidhiroki.ortelt@stud.uni-goettingen.de>
2023-09-04 17:36:03 +0200

    w

--- scripts/meis_egg.py
+++ scripts/meis_egg.py
@@ -232,7 +232,7 @@ if __name__ == "__main__":
                         )
                         end = time.time()
                         lambdas.append(energy_scale)
-                        energies.append(energy_fn(image))
+                        energies.append(energy_fn(image)["train"])
 
                         #SAVE IMAGES
 
